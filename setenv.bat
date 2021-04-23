f7ad8617807e9087f8eba2ff6e854ac2fb103165
KamilJankowski99 <kjankowski@wow.wroclaw.pl>
2021-04-23 19:47:39 +0200

    pipfreeze renamed to requirements.txt

--- setenv.bat
+++ setenv.bat
@@ -1,5 +1,5 @@
 CALL .\Scripts\activate.bat
 SET FLASK_APP=project
 SET FLASK_DEBUG=1
-pip install --upgrade --force-reinstall -r pipfreeze.txt
+pip install --upgrade --force-reinstall -r requirements.txt
 CALL flask run
