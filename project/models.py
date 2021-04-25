d32c407febe77cadc1685d4fff865700a6b09ea2
KamilJankowski99 <kjankowski@wow.wroclaw.pl>
2021-04-26 00:03:06 +0200

    obsługa czasu wysyłki

--- project/models.py
+++ project/models.py
@@ -17,3 +17,5 @@ class Mails(db.Model):
     email = db.Column(db.String(100))
     subject = db.Column(db.String(100))
     text = db.Column(db.String(1000))
+    date = db.Column(db.String(100))
+    time = db.Column(db.String(100))
